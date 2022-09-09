0fc552d440fecd481acc9f108a7066be9b261680
komal7023 <komalsharma70236>
2022-09-09 17:34:20 +0530

    added css

--- recruitment/forms.py
+++ recruitment/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import JobApplication, JobDescription, User
+from .models import JobApplication, User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm   
 
@@ -8,48 +8,32 @@ class AuthenticationFormWithInactiveUsersOkay(AuthenticationForm):
     def confirm_login_allowed(self, user):
         pass
 
-
 class UserForm (UserCreationForm): 
     username = forms.CharField(label = "Username")
     class Meta:
         model = User
-        fields = ('email','username')
+        fields = ('first_name','last_name','email','username')
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
         fields = ( "email",)        
-
-
-class ApplicationForm(forms.ModelForm):
-    contact_number = forms.CharField(max_length=12)
-    resume = forms.FileField(required=False)
-    notice_period = forms.IntegerField()
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-
-        try:
-            User.objects.get(email=email)
-        except User.DoesNotExist:
-            return email
-
-        raise forms.ValidationError('This email address is already in use.')    
+  
     
 class ApplicantForm(forms.ModelForm):
     def __init__(self, **kwargs):
 
         self.base_fields['user'].initial = kwargs.pop('user', None)
         self.base_fields['job_description'].initial = kwargs.pop('job_description', None)
+
         super(ApplicantForm, self).__init__(**kwargs)
         print(self.fields)
  
-    resume = forms.FileField()    
-
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20) 
+    resume = forms.FileField() 
+      
     class Meta:
         model = JobApplication
         widgets = {
@@ -57,10 +41,12 @@ class ApplicantForm(forms.ModelForm):
             "job_description": forms.HiddenInput(),
             "status": forms.HiddenInput(),
         }
-        fields = "__all__"
+        fields = "__all__" 
    
+    def save(self, commit=True, *args, **kwargs):
+        m = super(ApplicantForm, self).save(commit=False, *args, **kwargs)
+        if commit:
+            m.save()
+        return m    
 
-class JobDescriptionForm(forms.ModelForm):
-    class Meta:
-        model = JobDescription
-        fields = ('job_title','job_category','employment_type','organization') 
+   
